4f00758c430bb2a9a5a4fa1922fb4f6a86881057
Malone Hedges <me@malone.io>
2018-03-25 16:07:38 -0700

    Remove old code

--- app/src/main/java/io/malone/whattocook/MainActivity.java
+++ app/src/main/java/io/malone/whattocook/MainActivity.java
@@ -2,13 +2,8 @@ package io.malone.whattocook;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Parcelable;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,28 +30,6 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(mChannel);
         }
-    }
-
-    private void sendNotifiation() {
-        // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("https://www.google.com"));
-
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("Hello World")
-                .setContentText("I hope I get rendered on an emulator one day.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
-
     }
 
     @Override
